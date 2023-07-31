00728aa16b43cbb1e24607cad243ef4723ec70c1
aglantzrbc <adamglantz@yahoo.com>
2023-07-30 20:51:02 -0400

    commit initial files

--- Leaflet-Part-1_2/logic.js
+++ Leaflet-Part-1_2/logic.js
@@ -1,25 +1,25 @@
-// Import personal mapbox access token from config.js
+// Import the access token from the config.js file
 import { access_token } from "../config.js";
 
-// Store the API endpoint as queryUrl and tectonicplatesUrl variables
+// URL to fetch earthquake data
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// URL to fetch tectonic plate data
 var tectonicplatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Declare myMap variable outside the function scope to track if the map is already initialized
-var myMap = null; // myMap is null when the page is first loaded to avoid errors if the map is already initialized
-
-// Declare overlayMaps object to store overlay layers for the layer control
+// Variables to store the map and overlay layers
+var myMap = null;
 var overlayMaps = {
   "Earthquakes": null,
   "Tectonic Plates": null
-}; // overlayMaps is null when the page is first loaded to avoid errors if the map is already initialized
+};
 
-// Function to determine marker size based on magnitude of earthquake
+// Function to calculate the marker size based on magnitude
 function markerSize(magnitude) {
-  return magnitude * magnitude * 10000; // multiply by 10000 to make the markers more visible
-} // end of markerSize function
+  return magnitude * magnitude * 10000;
+}
 
-// Function to determine marker color by depth of earthquake 
+// Function to choose color based on depth
 function chooseColor(depth) {
   if (depth <= 10) return "#00FF00";
   else if (depth <= 30) return "greenyellow";
@@ -27,123 +27,117 @@ function chooseColor(depth) {
   else if (depth <= 70) return "orange";
   else if (depth <= 90) return "orangered";
   else return "#FF0000";
-} // end of chooseColor function
-
-function createFeatures(earthquakeData) {
-  // Define a function for each feature in the features array and bind a popup to each marker
-  // Give each feature a popup that describes the place, time, magnitude, and depth of the earthquake 
-  function onEachFeature(feature, layer) { 
-    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
-  } // end of onEachFeature function
-
-// Create a GeoJSON layer that contains the features array on the earthquakeData object and add it to the map
-// Run the onEachFeature function once for each piece of data in the array 
-var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature,
-
-    // Point to layer used to alter markers based on magnitude and depth of earthquake
-    pointToLayer: function (feature, latlng) {
-      // Determine the style of markers based on properties of the earthquake
-      var markers = {
-        radius: markerSize(feature.properties.mag),
-        fillColor: chooseColor(feature.geometry.coordinates[2]),
-        fillOpacity: 0.7,
-        color: "black",
-        stroke: true,
-        weight: 0.5,
-      };
-      return L.circle(latlng, markers); // return the markers to the GeoJSON layer
-    },
-  }); 
-
-// If map exists and the Earthquakes layer is enabled, clear its layers and keep it enabled on refresh
-if (myMap && myMap.hasLayer(overlayMaps["Earthquakes"])) {
-  overlayMaps["Earthquakes"].clearLayers();
-  earthquakes.addTo(myMap);
 }
 
-  // Update overlayMaps with the earthquakes layer 
-  overlayMaps["Earthquakes"] = earthquakes;
+// Function to create earthquake features and add them to the map
+async function createFeatures(earthquakeData) {
+  try {
+    // Function to bind a popup to each earthquake feature
+    function onEachFeature(feature, layer) {
+      // Bind a popup with information to each earthquake feature
+      layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+      // You can add more behavior here if needed, but don't return anything.
+    }
 
-  // Send our earthquakes layer to the createMap function to create the map
-  createMap(earthquakes);
+    // Create the earthquake layer with custom marker styling and popups
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature,
+      pointToLayer: function (feature, latlng) {
+        // Customize the marker properties based on earthquake magnitude and depth
+        var markers = {
+          radius: markerSize(feature.properties.mag),
+          fillColor: chooseColor(feature.geometry.coordinates[2]),
+          fillOpacity: 0.7,
+          color: "black",
+          stroke: true,
+          weight: 0.5,
+        };
+        return L.circle(latlng, markers);
+      },
+    });
+
+    // Remove the previous earthquakes layer from the map
+    if (myMap && myMap.hasLayer(overlayMaps["Earthquakes"])) {
+      myMap.removeLayer(overlayMaps["Earthquakes"]);
+    }
+
+    // Update the earthquakes overlay with the new data
+    overlayMaps["Earthquakes"] = earthquakes;
+    createMap(earthquakes); // Update the map with the new earthquakes layer
+  } catch (error) {
+    console.error("Error creating earthquake features:", error.message);
+  }
 }
 
-function createMap(earthquakes) {
-  // Create satellite tile layers for the map background using mapbox styles API
-  var satellite = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=" + access_token,
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      style: "mapbox/satellite-v9", // to comply with mapbox terms of service
-    }
-  ); 
+// Function to create the map and add earthquake and tectonic plates layers
+async function createMap(earthquakes) {
+  try {
+    // Create the different tile layers using Mapbox API
+    var satellite = L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" + access_token,
+      {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        id: "mapbox/satellite-v9",
+      }
+    );
 
-  // Create grayscale tile layers for the map background using mapbox styles API
-  var grayscale = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=" + access_token,
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      style: "mapbox/light-v11", // to comply with mapbox terms of service
-    }
-  );
-  // Create outdoors tile layers for the map background using mapbox styles API
-  var outdoors = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{style}/tiles/{z}/{x}/{y}?access_token=" + access_token,
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      style: "mapbox/outdoors-v12", // to comply with mapbox terms of service
-    }
-  );
+    var grayscale = L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" + access_token,
+      {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        id: "mapbox/light-v11",
+      }
+    );
 
-  // Create layer for tectonic plates to be added to the map
-  var tectonicPlates = new L.layerGroup();
+    var outdoors = L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" + access_token,
+      {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        id: "mapbox/outdoors-v12",
+      }
+    );
 
-  // d3.json() call to get the tectonic plates data from tectonicplatesUrl
-  d3.json(tectonicplatesUrl).then(function (plates) {
-    L.geoJSON(plates, {
-      color: "orange",
-      weight: 2,
-    }).addTo(tectonicPlates);
+    // Variable to store the tectonic plates layer
+    var tectonicPlates = new L.layerGroup();
 
-// If map exists and the Tectonic Plates layer is enabled, clear its layers and keep it enabled on refresh
-if (myMap && myMap.hasLayer(overlayMaps["Tectonic Plates"])) {
-  overlayMaps["Tectonic Plates"].clearLayers();
-  tectonicPlates.addTo(myMap);
-}
-
-    // Update overlayMaps with the tectonic plates layer 
+    // Fetch tectonic plate data and create the tectonic plates layer
+    const plates = await fetchTectonicPlates();
+    tectonicPlates = L.geoJSON(plates, {
+      style: {
+        color: "orange",
+        weight: 2,
+      },
+    });
     overlayMaps["Tectonic Plates"] = tectonicPlates;
 
-    // Create a baseMaps object to hold the base layers for the map
+    // Base maps for layer control
     var baseMaps = {
       Satellite: satellite,
       Grayscale: grayscale,
       Outdoors: outdoors,
     };
-  
-    // Check if myMap is not already initialized
+
     if (!myMap) {
-      // If map is not created yet, create it with both layers enabled
+      // Create the map for the first time
       myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 4,
-        layers: [satellite, earthquakes, tectonicPlates],
+        layers: [satellite, earthquakes],
       });
 
-      // Add a legend to the map to explain the marker colors for earthquake depth
+      // Add the layer control and legend
+      L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false,
+      }).addTo(myMap);
+
+      // Create and add the legend to the map
       var legend = L.control({ position: "bottomright" });
       legend.onAdd = function () {
         var div = L.DomUtil.create("div", "info legend"),
-          depth = [-10, 10, 30, 50, 70, 90]; // depth intervals for legend
+          depth = [-10, 10, 30, 50, 70, 90];
 
-        // Create a header for the legend
-          div.innerHTML += "<h3 style='text-align: center'>Depth</h3>";
+        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>";
 
-        // Loop through the depth intervals and generate a label with a colored square for each interval in the legend
         for (var i = 0; i < depth.length; i++) {
           div.innerHTML +=
             '<i style="background:' +
@@ -154,62 +148,73 @@ if (myMap && myMap.hasLayer(overlayMaps["Tectonic Plates"])) {
         }
         return div;
       };
-
-      // Add the legend to the map
       legend.addTo(myMap);
+    } else {
+      // Clear the map and re-add the updated layers
+      myMap.eachLayer(function (layer) {
+        myMap.removeLayer(layer);
+      });
 
-// Create the initial layer control
-L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false,
-}).addTo(myMap);
+      // Re-add the base layers
+      if (satellite) {
+        satellite.addTo(myMap);
+      }
 
-// Add the following code after the layer control creation
-let layerControlContainer = document.getElementsByClassName('leaflet-control-layers')[0];
-layerControlContainer.id = 'layerControl';
+      // Re-add the earthquake and tectonic plate layers based on the toggle box state
+      if (overlayMaps["Earthquakes"] && document.getElementById("earthquakeToggle")?.checked) {
+        myMap.addLayer(earthquakes);
+      }
 
-} else {
-  // Before creating a new layer control, remove the old one from the DOM
-  document.getElementById('layerControl').remove();
-
-  // If the map is already initialized, remove existing layers and add the new earthquakes layer to it
-  if (myMap.hasLayer(overlayMaps["Earthquakes"])) {
-      myMap.removeLayer(overlayMaps["Earthquakes"]);
+      // Check if the checkbox exists before accessing its checked property
+      var plateToggleCheckbox = document.getElementById("plateToggle");
+      if (overlayMaps["Tectonic Plates"] && plateToggleCheckbox && plateToggleCheckbox.checked) {
+        myMap.addLayer(tectonicPlates);
+      }
+    }
+  } catch (error) {
+    // Handle errors when fetching tectonic plate data or creating the map
+    console.error("Error:", error.message);
   }
-  if (myMap.hasLayer(overlayMaps["Tectonic Plates"])) {
-      myMap.removeLayer(overlayMaps["Tectonic Plates"]);
-  }
-
-  // Update the earthquakes and tectonic plates layers in the overlayMaps object 
-  overlayMaps["Earthquakes"] = earthquakes;
-  overlayMaps["Tectonic Plates"] = tectonicPlates;
-
-  // Add the new earthquakes and tectonic plates to the map 
-  earthquakes.addTo(myMap);
-  tectonicPlates.addTo(myMap);
-
-  // Create a new layer control
-  L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false,
-  }).addTo(myMap);
-
-  // Assign an id to the new layer control's DOM element
-  let layerControlContainer = document.getElementsByClassName('leaflet-control-layers')[0];
-  layerControlContainer.id = 'layerControl';
-}
-  });
 }
 
-// Function to fetch data from the API
-function fetchData() {
-  // d3.json() call to get the earthquake data from queryUrl
-  d3.json(queryUrl).then(function (data) {
-    // Once there is a response, send the data.features object to the createFeatures function
+// Function to fetch tectonic plate data
+async function fetchTectonicPlates() {
+  try {
+    const response = await fetch(tectonicplatesUrl);
+    
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // Handle errors when fetching tectonic plate data
+    console.error("Error fetching tectonic plate data:", error.message);
+    throw error; // Re-throw the error to be caught by the calling function
+  }
+}
+
+// Function to fetch earthquake data
+async function fetchData() {
+  try {
+    const response = await fetch(queryUrl);
+    
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    
+    const data = await response.json();
     createFeatures(data.features);
-  });
+  } catch (error) {
+    // Handle errors when fetching earthquake data
+    console.error("Error fetching earthquake data: ", error);
+    console.error("Please refresh the browser. Thank you.");
+  }
 }
 
-// Fetch initial data
-fetchData();
-
-// Update data every 5 minutes
-setInterval(fetchData, 300000); // 5 minutes = 300000 ms
+// Add a listener for the load event of the window object
+window.addEventListener("load", function () {
+  fetchData();
+  setInterval(fetchData, 300000); // 5 minutes = 300000 milliseconds
+});
